1cb5b61201096a06d05decae98e29730d0aa53a6
Noah <noah@noahs-mbp.lan>
2022-12-15 21:35:05 -0500

    Added some nil guards for socials

--- lib/mu/character/actions/social_action.ex
+++ lib/mu/character/actions/social_action.ex
@@ -17,8 +17,19 @@ defmodule Mu.Character.SocialAction do
     |> assign(:prompt, false)
   end
 
+  def run(conn, params = %{"character" => "self", "socials" => socials})
+      when socials.char_auto == nil or socials.others_auto == nil do
+    run(conn, %{params | "character" => nil})
+  end
+
+  def run(conn, params = %{"socials" => socials})
+      when socials.vict_found == nil or socials.others_found == nil do
+    run(conn, %{params | "character" => nil})
+  end
+
   def run(conn, params = %{"character" => "self"}) do
     params = %{params | "character" => conn.character.name}
+
     run(conn, params)
   end
 
