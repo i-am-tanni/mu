2bb6fd3e81ee0cc5cd584e3472eb5562ce10e1ab
Noah <noah@noahs-mbp.lan>
2022-12-17 21:46:01 -0500

    Reorganized Character.SocialAction to be more readable

--- lib/mu/character/actions/social_action.ex
+++ lib/mu/character/actions/social_action.ex
@@ -17,30 +17,30 @@ defmodule Mu.Character.SocialAction do
     |> assign(:prompt, false)
   end
 
-  def run(conn, params = %{"character" => "self", "socials" => socials})
-      when socials.char_auto == nil or socials.others_auto == nil do
-    run(conn, %{params | "character" => nil})
+  def run(conn, params = %{"character" => "self", "social" => social}) do
+    cond do
+      social.char_auto != nil and social.others_auto != nil ->
+        params = %{params | "character" => conn.character.name}
+        run(conn, params)
+
+      true ->
+        run(conn, %{params | "character" => nil})
+    end
   end
 
-  def run(conn, params = %{"socials" => socials})
-      when socials.vict_found == nil or socials.others_found == nil do
-    run(conn, %{params | "character" => nil})
-  end
-
-  def run(conn, params = %{"character" => "self"}) do
-    params = %{params | "character" => conn.character.name}
-
-    run(conn, params)
-  end
-
-  def run(conn, params) do
+  def run(conn, %{"character" => character, "social" => social})
+      when social.vict_found != nil and social.others_found != nil do
     params = %{
-      name: params["character"],
-      text: params["social"]
+      name: character,
+      text: social
     }
 
     conn
     |> event("social/send", params)
+  end
+
+  def run(conn, params) do
+    run(conn, %{params | "character" => nil})
   end
 
   defp meta(character: character) do
