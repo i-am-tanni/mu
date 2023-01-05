4c7d777a1afadd9f7d0dc25c226509d451c33ed2
“Tanni” <noah@noahs-mbp.lan>
2023-01-05 06:55:00 -0500

    MoveCommand less verbose

--- lib/mu/character/commands/move_command.ex
+++ lib/mu/character/commands/move_command.ex
@@ -1,39 +1,27 @@
 defmodule Mu.Character.MoveCommand do
   use Kalevala.Character.Command
 
-  def north(conn, _params) do
+  def run(conn, params) do
+    direction = params["command"]
+
     conn
-    |> request_movement("north")
+    |> request_movement(to_long(direction))
     |> assign(:prompt, false)
   end
 
-  def south(conn, _params) do
-    conn
-    |> request_movement("south")
-    |> assign(:prompt, false)
-  end
+  defp to_long(direction) do
+    case String.length(direction) < 3 do
+      true ->
+        case direction do
+          "n" -> "north"
+          "s" -> "south"
+          "e" -> "east"
+          "w" -> "west"
+          _ -> direction
+        end
 
-  def east(conn, _params) do
-    conn
-    |> request_movement("east")
-    |> assign(:prompt, false)
-  end
-
-  def west(conn, _params) do
-    conn
-    |> request_movement("west")
-    |> assign(:prompt, false)
-  end
-
-  def up(conn, _params) do
-    conn
-    |> request_movement("up")
-    |> assign(:prompt, false)
-  end
-
-  def down(conn, _params) do
-    conn
-    |> request_movement("down")
-    |> assign(:prompt, false)
+      false ->
+        direction
+    end
   end
 end
