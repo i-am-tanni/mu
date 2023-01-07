182fb000412c496ebf333234ac524d64ba346ea4
Noah <noah@noahs-mbp.lan>
2023-01-06 20:56:02 -0500

    Breadth First Search pathfinding added

---
+++ lib/mu/character/commands/path_find_command.ex
@@ -0,0 +1,35 @@
+defmodule Mu.Character.PathFindCommand do
+  use Kalevala.Character.Command
+
+  alias Mu.Character.PathFindData
+
+  def run(conn, params) do
+    room_id = conn.character.room_id
+    id = Kalevala.Character.generate_id()
+
+    path_find_data = %PathFindData{
+      id: id,
+      text: params["text"],
+      visited: MapSet.new() |> MapSet.put(room_id),
+      lead_count: 1,
+      status: :continue,
+      created_at: DateTime.utc_now()
+    }
+
+    params = %{
+      id: id,
+      success: false,
+      text: params["text"],
+      steps: [],
+      room_exits: [],
+      depth: 0,
+      max_depth: 10,
+      topic: "room/track"
+    }
+
+    conn
+    |> put_flash(:path_find_data, path_find_data)
+    |> event("room/pathfind", params)
+    |> assign(:prompt, false)
+  end
+end
