0105d87cb4c371a4de116c2627e5582cf6d4eea9
“Tanni” <default-user@default-mail.com>
2023-01-29 05:43:03 -0500

    Added nil guard for end_room_pid

--- lib/mu/world/room/events/door_event.ex
+++ lib/mu/world/room/events/door_event.ex
@@ -1,5 +1,6 @@
 defmodule Mu.World.Room.DoorEvent do
   import Kalevala.World.Room.Context
+  require Logger
   alias Mu.World.Exit
 
   def call(context, event) do
@@ -60,12 +61,15 @@ defmodule Mu.World.Room.DoorEvent do
   end
 
   defp pass(context, event) when context.data.id == event.data.start_room_id do
-    end_room_id = event.data.end_room_id
+    end_room_pid =
+      event.data.end_room_id
+      |> Kalevala.World.Room.global_name()
+      |> GenServer.whereis()
 
-    end_room_id
-    |> Kalevala.World.Room.global_name()
-    |> GenServer.whereis()
-    |> send(event)
+    case !is_nil(end_room_pid) do
+      true -> send(end_room_pid, event)
+      false -> Logger.error("Room #{event.data.end_room_id} doesn't exist to receive DoorEvent.")
+    end
 
     context
   end
