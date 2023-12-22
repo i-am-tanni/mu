d2999356885ae86585928534374e052d6f95315f
Tanni <tannisroot@proton.me>
2023-12-22 15:12:59 -0500

    reorg

--- lib/mu/character/commands/combat_command.ex
+++ lib/mu/character/commands/combat_command.ex
@@ -1,13 +1,15 @@
 defmodule Mu.Character.CombatCommand do
   use Kalevala.Character.Command
 
+  alias Mu.Character
   alias Mu.Character.CombatAction
 
   def request(conn, params) do
     text = params["text"]
+    data = Character.build_attack(conn, text)
 
     conn
-    |> CombatAction.run(%{text: text})
+    |> CombatAction.run(data)
     |> assign(:prompt, false)
   end
 end
