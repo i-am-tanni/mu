fac61cf7418f353d99b379453702fbf77ff47979
Tanni <tannisroot@proton.me>
2024-03-06 21:23:12 -0500

    Moved Hunt Event

---
+++ lib/mu/character/events/hunt_event.ex
@@ -0,0 +1,65 @@
+defmodule Mu.Character.HuntEvent do
+  @moduledoc """
+  For Non-players hunting characters
+  Callable via the HuntController, which is used by non-players only
+  """
+
+  use Kalevala.Character.Event
+  import Mu.Character.Guards
+
+  alias Mu.Character
+  alias Mu.Character.PathController
+
+  # Actions
+  alias Mu.Character.WanderAction
+  alias Mu.Character.ListAction
+  alias Mu.Character.CombatAction
+
+  def call(conn, event) when not in_combat(conn) do
+    target_id = get_flash(conn, :target_id)
+    expires_at = get_flash(conn, :expires_at)
+
+    # As long as the hunt hasn't expired
+    case Time.compare(Time.utc_now(), expires_at) == :lt do
+      true ->
+        # If quarry is in the room
+        case Enum.find(event.data.characters, &Character.matches?(&1, target_id)) do
+          # attack
+          %{} ->
+            attack_data = Character.build_attack(conn, target_id)
+            CombatAction.put(conn, attack_data)
+
+          # ...or continue hunting
+          nil ->
+            conn
+            |> WanderAction.put(%{}, pre_delay: 3000)
+            |> ListAction.put(%{type: :characters})
+        end
+
+      false ->
+        # but if expired, path home
+        threat_table = get_meta(conn, :threat_table)
+        return_path = get_flash(conn, :return_path)
+        destination = get_flash(conn, :room_id)
+
+        data = %PathController{
+          path: return_path,
+          destination_id: destination
+        }
+
+        conn
+        |> put_meta(:threat_table, refresh(threat_table))
+        |> put_controller(PathController, data)
+    end
+  end
+
+  def call(conn, _), do: conn
+
+  defp refresh(threat_table) do
+    threat_table
+    |> Enum.filter(fn {_, %{expires_at: expires_at}} ->
+      Time.compare(Time.utc_now(), expires_at) == :lt
+    end)
+    |> Enum.into(%{})
+  end
+end
