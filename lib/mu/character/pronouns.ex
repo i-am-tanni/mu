1b701714406415c8827db6e71a9447ea390f5fe8
tanni <tannisroot@proton.me>
2024-06-18 19:28:23 -0400

    Code reorganization

--- lib/mu/character/pronouns.ex
+++ lib/mu/character/pronouns.ex
@@ -1,26 +1,24 @@
 defmodule Mu.Character.Pronouns do
+
+  @male %{
+    subject: "he",
+    object: "him",
+    possessive: "his",
+    reflexive: "himself"
+  }
+
+  @female %{
+    subject: "she",
+    object: "her",
+    possessive: "her",
+    reflexive: "herself"
+  }
+
   def get(pronouns) do
     case pronouns do
-      :male -> male()
-      :female -> female()
+      :male -> @male
+      :female -> @female
     end
   end
 
-  defp male() do
-    %{
-      subject: "he",
-      object: "him",
-      possessive: "his",
-      reflexive: "himself"
-    }
-  end
-
-  defp female() do
-    %{
-      subject: "she",
-      object: "her",
-      possessive: "her",
-      reflexive: "herself"
-    }
-  end
 end
